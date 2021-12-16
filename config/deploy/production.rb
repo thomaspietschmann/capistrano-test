415033d6ab5a3cc5df5a06ae0854553967bef576
Thomas Pietschmann <thomas@pietschi.com>
2021-12-16 16:19:55 +0100

    add staging config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,6 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 
 
 # role-based syntax
